bb59331c77804d32d722f35671302d7d237c1a78
Ahmed Shehata <Ahmed.Shehata.Rasmy5@gmail.com>
2025-08-04 14:43:44 +0300

    Fea/ Add all colors to the project
    Add Primary, Secondary, Label Colors to Colors assets

--- sooq/sooq/SplashViewController.swift
+++ sooq/sooq/SplashViewController.swift
@@ -13,6 +13,7 @@ class SplashViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+       // view.backgroundColor = UIColor.primaryOrangeFresh
     }
 
 

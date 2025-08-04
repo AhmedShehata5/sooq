8d94f6c74863b6700ac615d02c5a8cc6c417a79d
Ahmed Shehata <Ahmed.Shehata.Rasmy5@gmail.com>
2025-08-05 01:09:44 +0300

    Feat/ Add Colors generated file
    Use SwiftGen to to create a file with all of color's neames

---
+++ Sooq_UI/Sources/Sooq_UI/Generated/Colors.Generated.swift
@@ -0,0 +1,26 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+
+import UIKit
+
+
+extension UIColor {
+    
+        static let labelOffBlue = UIColor(named: "Label Off Blue")
+        static let labelOffGreen = UIColor(named: "Label Off Green")
+        static let labelOffRed = UIColor(named: "Label Off Red")
+        static let primaryBlueOcean = UIColor(named: "Primary Blue Ocean")
+        static let primaryNavyBlack = UIColor(named: "Primary Navy Black")
+        static let primaryOrangeFresh = UIColor(named: "Primary Orange Fresh")
+        static let primaryPureWhite = UIColor(named: "Primary Pure White")
+        static let secondaryDarkGrey = UIColor(named: "Secondary Dark Grey")
+        static let secondaryEarthGreen = UIColor(named: "Secondary Earth Green")
+        static let secondaryHalfGrey = UIColor(named: "Secondary Half Grey")
+        static let secondaryOffGrey = UIColor(named: "Secondary Off Grey")
+        static let secondaryRedVelvet = UIColor(named: "Secondary Red Velvet")
+        static let secondarySoftGrey = UIColor(named: "Secondary Soft Grey")
+  
+    
+}
+
